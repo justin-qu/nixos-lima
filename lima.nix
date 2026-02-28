5d193301d9d13c2f59c014b77e9484c826a7a200
Justin Qu <justin.qu@saronic.com>
2026-02-28 15:08:42 -0600

    feat(nix): update NixOS image to use Determinate Nix, Rosetta 2 virtuallizations, format flake

--- lima.nix
+++ lima.nix
@@ -1,55 +1,82 @@
-{ config, modulesPath, pkgs, lib, ... }:
 {
-    imports = [
-        (modulesPath + "/profiles/qemu-guest.nix")
-        ./lima-init.nix
+  config,
+  modulesPath,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./lima-init.nix
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Get image under 2GB for Github release.
+  documentation.enable = false;
+
+  # Give users in the `wheel` group additional rights when connecting to the Nix daemon
+  # This simplifies remote deployment to the instance's nix store.
+  nix.settings.trusted-users = [ "@wheel" ];
+
+  # Read Lima configuration at boot time and run the Lima guest agent
+  services.lima.enable = true;
+
+  # ssh
+  services.openssh.enable = true;
+
+  security = {
+    sudo = {
+      enable = true;
+      wheelNeedsPassword = false;
+    };
+  };
+
+  # system mounts
+  boot = {
+    kernelParams = [ "console=tty0" ];
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+    };
+  };
+  fileSystems = {
+    "/".options = [
+      "discard"
+      "noatime"
+      "nodiratime"
     ];
-
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    # Give users in the `wheel` group additional rights when connecting to the Nix daemon
-    # This simplifies remote deployment to the instance's nix store.
-    nix.settings.trusted-users = [ "@wheel" ];
-
-    # Read Lima configuration at boot time and run the Lima guest agent
-    services.lima.enable = true;
-
-    # ssh
-    services.openssh.enable = true;
-
-    security = {
-        sudo.wheelNeedsPassword = false;
-    };
-
-    # system mounts
-    boot = {
-        kernelParams = [ "console=tty0" ];
-        loader.grub = {
-            device = "nodev";
-            efiSupport = true;
-            efiInstallAsRemovable = true;
-        };
-    };
-    fileSystems."/boot" = {
-        device = lib.mkForce "/dev/vda1";  # /dev/disk/by-label/ESP
-        fsType = "vfat";
-    };
-    fileSystems."/" = {
-        device = "/dev/disk/by-label/nixos";
-        autoResize = true;
-        fsType = "ext4";
-        options = [ "noatime" "nodiratime" "discard" ];
-    };
-
-    # misc
-    boot.kernelPackages = pkgs.linuxPackages_latest;
-
-    # pkgs
-    environment.systemPackages = with pkgs; [
-        vim
-        git
+    "/boot".options = [
+      "discard"
+      "noatime"
+      "umask=0077"
     ];
+  };
 
-    system.stateVersion = "25.11";
+  # pkgs
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    htop
+    direnv
+    nix-output-monitor
+  ];
+
+  programs = {
+    direnv = {
+      enable = true;
+      silent = true;
+    };
+  };
+
+  system.stateVersion = "25.11";
+
+  virtualisation.rosetta = {
+    enable = true;
+    mountTag = "vz-rosetta";
+  };
 }
-
